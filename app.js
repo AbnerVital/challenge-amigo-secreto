6cedb8dc4f477c7d3c27acc6a81b4eba4032fc97
Abner Vital <avgchaves@gmail.com>
2025-02-04 05:56:43 -0300

    implementado a função para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -7,8 +7,19 @@ function adicionarAmigo() {
     } else {
         amigos.push(nome);
         limparCampo();
-        
+        exibirAmigos(amigos);
     }       
+}
+
+function exibirAmigos(amigos) {
+    const lista = document.getElementById('listaAmigos'); // Seleciona o elemento <ul> pelo ID
+    lista.innerHTML = ''; // Limpa o conteúdo anterior da lista
+
+    amigos.forEach(function(amigo) {
+        const item = document.createElement('li'); // Cria um novo elemento <li>
+        item.textContent = amigo; // Define o texto do <li> com o nome do amigo
+        lista.appendChild(item); // Adiciona o <li> como filho da <ul>
+    });
 }
 
 function limparCampo() {
